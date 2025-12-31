5c4189ad01b796aab594541638c054f48ff99c11
harapriyax <harapriyarout623@gmail.com>
2025-12-31 15:18:14 +0530

    Create AboutUs.jsx

---
+++ AboutUs.jsx
@@ -0,0 +1,13 @@
+function AboutUs() {
+  return (
+    <div>
+      <h2>About Paradise Nursery</h2>
+      <p>
+        Paradise Nursery provides high-quality indoor and outdoor plants
+        to enhance your living space.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
